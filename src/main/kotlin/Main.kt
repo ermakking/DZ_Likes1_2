1a1f216644a48c735ced91c32eca1bdb94a08447
Ermakking <ermakking@yandex.ru>
2023-05-15 22:58:01 +0400

    correction of remarks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ println("Введите количество лайков:")
 
 
 
-    if (likes == 11 || likes % 10 != 1 || likes % 100 == 11) {
+    if (likes % 10 != 1 || likes % 100 == 11) {
         println("Понравилась людям " + likes)
     } else {
         println("Понравилась человеку " + likes)
